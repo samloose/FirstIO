022268430d42eaea94451d5206b6592f7e3e3919
samloose <sloose@jaguar.csuchico.edu>
2018-09-05 16:24:20 -0700

    Changes the greeeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
+//Author : Sam Loose 
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Sam. What's yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
